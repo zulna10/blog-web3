6c01ad383516ed68dcc85561e753fb48de9f2f19
zulkar nain <zulkarnain10062001@gmail.com>
2024-08-04 21:38:03 +0000

    add

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
-  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Travel.";
+  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Blog Web3";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Samantha";
   const defaultTitle =
     process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Travel with Samantha";
